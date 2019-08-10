027faa404dd5b0073f060be5213ebedffb75dc04
Vasanthkumar V <vasanthkumarvelusamy@gmail.com>
2019-08-10 15:21:55 +0530

    Initial Commit

---
+++ Logger.framework/Headers/Logger.h
@@ -0,0 +1,19 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by 1741103 on 09/08/19.
+//  Copyright © 2019 1741103. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
